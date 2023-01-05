9c7c1fce9a9cf777fe76a8298294d65bbe8b277e
Neon17 <neon.neupane.9@gmail.com>
2023-01-05 17:29:14 +0545

    Beautiful Change in Scrolling of Stories in News Feed

--- Facebook.js
+++ Facebook.js
@@ -6,5 +6,78 @@ window.onload = function () {
   LeftPage.style.height = y;
   RightPage.style.height = y;
   console.log(y);
-  var Fixed = document.getElementsByClassName("FixedPageStart");
+  var Fixed = document.getElementsByClassName("FixedPageStart");  
+
+  AllStories = document.getElementsByClassName("StoriesinNewsFeed");
+  CountStories = AllStories.length;
+  for (var i = 0; i < CountStories; i++){
+    if ((i>=0) && (i<=4)){
+      AllStories[i].style.display = "flex";  
+    }
+    else {
+      AllStories[i].style.display = "none";
+    }
+  }
 };
+
+/*Current Start is the News Feed Stories Scrolling*/
+var CurrentStart = 0;
+var CurrentEnd = 4;
+var CountStories; //To find the number of total stories
+
+function LeftAnchorClick () {
+  AllStories = document.getElementsByClassName("StoriesinNewsFeed");
+  CountStories = AllStories.length;
+  if (CurrentStart > 0){
+    CurrentStart -= 4;
+    CurrentEnd -=4;
+    for (var i = 0; i < CountStories; i++){
+      if ((i>=CurrentStart) && (i<=CurrentEnd)){
+        AllStories[i].style.display = "flex";  
+      }
+      else {
+        AllStories[i].style.display = "none";
+      }
+    }
+  }
+}
+function RightAnchorClick () {
+  AllStories = document.getElementsByClassName("StoriesinNewsFeed");
+  CountStories = AllStories.length;
+  if (CurrentEnd < (CountStories-1)){
+    CurrentStart += 4;
+    CurrentEnd +=4;
+    for (var i = 0; i < CountStories; i++){
+      if ((i>=CurrentStart) && (i<=CurrentEnd)){
+        AllStories[i].style.display = "flex";  
+      }
+      else {
+        AllStories[i].style.display = "none";
+      }
+    }
+  }
+}
+
+
+function BlueBarFirst() {
+  BlueBarReset(); /* To make all remaining Blue Bars black if not clicked */
+  var Line = document.getElementsByClassName("LineDrawHolderMain")[0];
+  Line.style.backgroundColor="blue";
+}
+function BlueBarSecond() {
+  BlueBarReset();
+  var Line = document.getElementsByClassName("LineDrawHolderMain")[1];
+  Line.style.backgroundColor="blue";
+}
+function BlueBarThird() {
+  BlueBarReset();
+  var Line = document.getElementsByClassName("LineDrawHolderMain")[2];
+  Line.style.backgroundColor="blue";
+}
+
+function BlueBarReset () {
+  var Line = document.getElementsByClassName("LineDrawHolderMain");
+  Line[0].style.backgroundColor = "black";
+  Line[1].style.backgroundColor = "black";
+  Line[2].style.backgroundColor = "black";
+}
